3169e2acfb64bfbd6ef69e096315ec90383a50f4
akhsak <akhsamariyam2k2@gmail.com>
2023-10-17 11:56:55 +0530

    first commit

---
+++ sum_positive.dart
@@ -0,0 +1,15 @@
+
+//you get an array of numbers, return the sum of all of the positives ones.
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+void main(List<String> args) {
+int positiveSum(arr){
+  int sum = 0;
+  for (int number in arr) {
+    if (number > 0) {
+      sum += number;
+    }
+  }
+  return sum;
+}
+  }
