71d40d071cfd7c2cf1280cfbd368a86bc2a6f836
akhsak <akhsamariyam2k2@gmail.com>
2023-10-28 11:53:34 +0530

    add new program

---
+++ string_Uppercase.dart
@@ -0,0 +1,18 @@
+ //Create a method to see whether the string is ALL CAPS.
+
+//Examples (input -> output)
+// "hi" -> False
+// "HI" -> True
+ 
+ bool isUpperCase(String str) {
+  return str == str.toUpperCase();
+}
+void main(List<String> args) {
+  
+
+  String str1 = "hi";
+  String str2 = "HI";
+  print(isUpperCase(str1));
+  print(isUpperCase(str2));
+  
+}
